2b7701319e01126e359e48f223668d369aad6929
Katya-Korotenko <korotenkok99943@gmail.com>
2026-05-29 21:33:01 +0200

    Добавила поиск по ключевому слову, по жанру, по году. Перед выбором жанра выводится весь список жанров в базе. Перед выбором года выводится минимальный и максимальный год. Пейджирование по 10 фильмов через offset

--- ui.py
+++ ui.py
@@ -1,57 +1,79 @@
 import sys
 
-from mysql_connector import get_cursor
-from sql_queries import SEARCH_TITLE_QUERY
+from dns.entropy import between
 
+from movie_searcher import MovieSearcher
 
-class MovieSearcher:
-
-    def execute_query(self, query, parameter = None):
-        with get_cursor() as cursor:
-            cursor.execute(query, parameter)
-            return cursor.fetchall()
-
-    def search_by_keyword(self):
-        choice = input("What movie do you want to search?: ")
-        result = self.execute_query(SEARCH_TITLE_QUERY, (f"%{choice.upper()}%",))
-        return (row for row in result)
-
-
-    # в процессе реализации
-    def search_by_gener(self):
-        print("Search by gener")
-
-
-    # в процессе реализации
-    def top_searches(self):
-        print("Top searches")
 
 class Menu(MovieSearcher):
 
     def __init__(self):
         self.main_options = {
-            "1": ("Search film.", self.show_submenu_for_movies),
-            "2": ("Top searches films.", self.top_searches),
-            "0": ("Exit", lambda: sys.exit(0)),
-        }
+            "1": ("Search movies.", self.show_submenu_for_movies),
+            "2": ("Top searches movies.", self.top_searches),
+            "0": ("Exit", lambda: sys.exit(0)),}
 
     def show_submenu_for_movies(self):
         sub_options = {
-            "1": ("Search film by keyword.", lambda: self.print_films(self.search_by_keyword())),
-            "2": ("Search film by gener.", self.search_by_gener),
-            "0": ("Back", lambda: self.show_menu(self.main_options)),
-        }
+            "1": ("Search movies by keyword.",self.handle_keyword_search),
+            "2": ("Search movies by genre.", self.handle_genre_search),
+            "3": ("Search movies by year ",self.year_range),
+            "0": ("Back", lambda: self.show_menu(self.main_options)),}
         self.show_menu(sub_options)
 
-    def print_films(self, movies):
+    # поиск по выбраному году (пока наработка)
+    def year_range(self):
+        years = self.get_year_range()
+        for year in years:
+            print(f"Choose a year range between {year['min']} and {year['max']}")
+        input_year = input("Enter year: ")
+        movies = self.search_by_year(input_year, offset=0)
+        if input_year.isdigit():
+            self.print_films(movies, lambda offset: self.search_by_year(input_year, offset))
+    #  поиск по жанру
+    def handle_genre_search(self):
+        genres = self.get_genres()
+        selected = self.print_genres(genres)
+        movies = self.search_by_genre(selected, offset=0)
+        self.print_films(movies, lambda offset: self.search_by_genre(selected, offset))
+    #поиск по ключевому слову
+    def handle_keyword_search(self):
+        choice = input("What movie do you want to search?: ")
+        movies = self.search_by_keyword(choice, offset=0)
+        self.print_films(movies, lambda offset: self.search_by_keyword(choice, offset))
+
+    # вывод всех жанров
+    def print_genres(self, genres):
+        print("===Genres===")
+        for index, gen in enumerate(genres, start=1):
+            print(f"{index}. {gen['name']}")
+
+        choice = input("Select genre number: ")
+        if choice.isdigit() and 1 <= int(choice) <= len(genres):
+            return genres[int(choice) - 1]['name']
+        print("Invalid choice")
+        return self.print_genres(genres)
+
+    # выводит фильмы
+    def print_films(self, movies, search_func, offset=0):
         print("===Result===")
         if not movies:
-            print("No films found.")
+            print("Movies not found.")
             self.show_menu(self.main_options)
         for movie in movies:
-            print(f"{movie['title']}({movie['release_year']})---{movie['description']}")
+            print(f"{movie['title']}({movie['release_year']})---{movie['name']}---{movie['description']}")
+
+        if len(movies) >= 10:
+            choice = input("Back to menu [0] | Next 10 movies [n]").lower()
+            if choice == 'n':
+                next_movies = search_func(offset + 10)
+                self.print_films(next_movies, search_func, offset + 10)
+            self.show_menu(self.main_options)
+        self.show_menu(self.main_options)
 
 
+
+    # выводит меню
     @staticmethod
     def show_menu(options):
 
