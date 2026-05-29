285d6842b354a617a14ba44f949a8670a867857e
Katya-Korotenko <korotenkok99943@gmail.com>
2026-05-29 21:35:31 +0200

    Создала новый файл для работы с запросами

---
+++ movie_searcher.py
@@ -0,0 +1,22 @@
+from mysql_connector import execute_query
+from sql_queries import *
+
+class MovieSearcher:
+
+    def search_by_keyword(self, choice, offset=0):
+        return execute_query(SEARCH_TITLE_QUERY, (f"%{choice}%", offset,))
+
+    def get_genres(self):
+        return execute_query(ALL_GENERS_QUERY)
+
+    def get_year_range(self):
+        return execute_query(MIN_MAX_YEAR_QUERY)
+
+    def search_by_year(self, year, offset = 10):
+        return execute_query(YEAR_RANGE_QUERY,(year, offset))
+
+    def search_by_genre(self, genre,  offset=0):
+        return execute_query(CHOICE_GENRE_QUERY, (genre, offset,))
+
+
+
