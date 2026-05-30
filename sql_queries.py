e18ac65940c803d9f4af4889b6e9a650ec2c0cb7
Katya-Korotenko <korotenkok99943@gmail.com>
2026-05-30 09:58:23 +0200

    запрос теперь может в диапазоне нужных лет искать фильмы

--- sql_queries.py
+++ sql_queries.py
@@ -13,12 +13,12 @@ MIN_MAX_YEAR_QUERY = """
                      from film;"""
 
 YEAR_RANGE_QUERY = """
-                     SELECT f.title, f.description, f.release_year, c.name
-                     FROM film f
-                              LEFT JOIN film_category fc ON fc.film_id = f.film_id
-                              LEFT JOIN category c ON c.category_id = fc.category_id
-                     WHERE f.release_year LIKE %s LIMIT 10
-                     OFFSET %s;"""
+                   SELECT title, description, release_year, c.name
+                   from film f
+                            join film_category fc on fc.film_id = f.film_id
+                            join category c on c.category_id = fc.category_id
+                   where release_year between %s and %s limit 10
+                   OFFSET %s;"""
 
 ALL_GENERS_QUERY = """
                    SELECT DISTINCT name
