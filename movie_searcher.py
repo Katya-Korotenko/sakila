c60964276d84577684d63b1837c3380b402876a3
Katya-Korotenko <korotenkok99943@gmail.com>
2026-06-02 18:20:48 +0200

    исправила ошибку

--- movie_searcher.py
+++ movie_searcher.py
@@ -12,7 +12,7 @@ class MovieSearcher:
     def get_year_range(self):
         return execute_query(MIN_MAX_YEAR_QUERY)
 
-    def search_by_year(self, year_from, year_to, offset = 10):
+    def search_by_year(self, year_from, year_to, offset = 0):
         return execute_query(YEAR_RANGE_QUERY,(year_from, year_to, offset))
 
     def search_by_genre(self, genre,  offset=0):
